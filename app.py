10b9700825b9cc34b4cc3395f945410fed68b604
Uvejs Gjelaj <61552503+UvejsGj@users.noreply.github.com>
2025-05-01 18:40:14 +0200

    app.py
    learning

---
+++ app.py
@@ -0,0 +1,59 @@
+# print("Hello World")
+
+# x = 10
+# y = 10
+# print(x * y)
+
+# students_count = 1000
+# rating = 4.99
+# is_published = False 
+# course_name = "Python Programming" 
+# print(students_count)     
+
+# course = "Python Programming" 
+# print(len(course)) 
+# print(course[0])
+# print(course[-1])
+# print(course[0:3])  
+# print(course[0:])
+# print(course[:3])
+# print(course[:])
+
+
+# escape_sequences in python 
+# \"
+# \'
+# \\
+# \n
+
+# course = "Python \nProgramming"
+# print(course)
+
+# first = "Uvejs"
+# last = "Gjelaj"
+# full = f"{len(first)} {3+6}"
+# print(full)
+
+# course = "python programming  "
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+# print(course.strip())
+# print(course.find("Pro"))
+# print(course.replace("p", "J"))
+
+# x = 1
+# x = 1.1
+# x = 1 +2j 
+
+# print(10 + 3)
+# print(10 - 3)
+# print(10 * 3)
+# print(10 / 3)
+# print(10 // 3)
+# print(10 % 3)
+# print(10 ** 3)
+
+# x = 10
+# x = x + 3
+# x += 3
